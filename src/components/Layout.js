184dd3cf6ca657fecf395454a22ecb9324cc5edf
Md. Omar Shahariar <omar.shahariar@reddotdigitalit.com>
2023-02-27 15:39:42 +0600

    first commit

---
+++ src/components/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Layout = () => {
+    return (
+        <div className='bg-neutral'><Navbar></Navbar></div>
+    );
+};
+
+export default Layout;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div class="navbar bg-neutral  container mx-auto">
+  <div class="flex-1">
+    <a href="##" class="font-serif normal-case text-xl text-white">React Spring Boot</a>
+  </div>
+  <div class="flex-none gap-2">
+    <div class="form-control">
+      <input type="text" placeholder="Search" class="input input-bordered" />
+    </div>
+   
+  </div>
+</div>
+  );
+};
+
+export default Navbar;
